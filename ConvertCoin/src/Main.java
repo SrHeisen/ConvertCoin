ffc46550a8aa3ec860c1ef81b0158f277a8f7e98
Kevin Ramirez <keviiproxx123@gmail.com>
2022-12-01 22:30:01 -0500

    ConvertCoins V1.2

--- ConvertCoin/src/Main.java
+++ ConvertCoin/src/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        JOptionPane.showMessageDialog(null,"Hi, Welcome CovertCoin!");
+
+        String name = JOptionPane.showInputDialog("Registra tu nombre en ConverCoin!");
+
+        JOptionPane.showMessageDialog(null, "Welcome, " + name);
+
+        
     }
 }
