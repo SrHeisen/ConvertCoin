e15a6a648a5ab58a7fe74ab0ef87f951e34d1b4f
Kevin Ramirez <keviiproxx123@gmail.com>
2022-12-01 22:59:29 -0500

    ConvertCoins V1.4

--- ConvertCoin/src/Main.java
+++ ConvertCoin/src/Main.java
@@ -3,12 +3,22 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
 
-        JOptionPane.showMessageDialog(null,"Hi, Welcome CovertCoin!");
+        JOptionPane.showMessageDialog(null, "Hi, Welcome CovertCoin!");
 
         String name = JOptionPane.showInputDialog("Registra tu nombre en ConverCoin!");
 
         JOptionPane.showMessageDialog(null, "Welcome, " + name);
+        JOptionPane.showMessageDialog(null, name + " ConvertCoin es una plataforma donde te vamos ayudar a obtener la convercion de lo que elijas!");
 
-        
+        String [] Convercion ={"Temperatura","Moneda","Distancia"};
+
+        Object opcion = JOptionPane.showInputDialog(null,"Selecciona una Convercion" + name, "Elegir",JOptionPane.QUESTION_MESSAGE,null, Convercion, Convercion[0]);
+
+        switch (Convercion){
+            case Temperatura:
+                do{
+
+                }
+        }
     }
 }
